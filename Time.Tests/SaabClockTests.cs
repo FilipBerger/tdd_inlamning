0aa82bb7ba5ae03495e81578c1bcdd0efbdab2bf
Filip <filip-berger@hotmail.com>
2024-03-06 11:27:20 +0100

    Green: First test passes with simplest possible implementation.

--- Time.Tests/SaabClockTests.cs
+++ Time.Tests/SaabClockTests.cs
@@ -1,32 +1,28 @@
 using tdd_inlamning;
 
-namespace Times.Tests
+namespace tdd.inlamning.Tests
 {
-    public class TimeTests
+    public class SaabClockTests
     {
         [Fact]
         public void ValidTimeFormat_Test()
         {
             // Arrange
-            var time = new Time();
+            var saabClock = new SaabClock();
 
             var secondsInput = 53;
             var minutesInput = 30;
             var hoursInput = 12;
 
-            var expectedResponse = new DateTime(12,30,53);
+            var expectedResponse = new TimeOnly(12,30,53);
 
             // Act
 
-            var response = time.GetTime(secondsInput, minutesInput, hoursInput);
+            var response = saabClock.GetTime(hoursInput, minutesInput, secondsInput);
 
             // Assert
 
             Assert.Equal(expectedResponse, response);
-
-            //var validTime = new ValidTime();
-            
-
         }
     }
 }
---
+++ tdd_inlamning/SaabClock.cs
@@ -0,0 +1,12 @@
+﻿
+namespace tdd_inlamning
+{
+    public class SaabClock
+    {
+        public TimeOnly GetTime(int hoursInput, int minutesInput, int secondsInput)
+        {
+            var time = new TimeOnly(hoursInput, minutesInput, secondsInput);
+            return time;
+        }
+    }
+}
